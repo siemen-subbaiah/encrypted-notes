f7c673c6817dfa0190ba72b084897fe3498f3c09
siemen_subbaiah <siemen.s@iqnext.io>
2023-08-04 11:12:03 +0530

    Toast util added

--- app/components/createWebSearch.tsx
+++ app/components/createWebSearch.tsx
@@ -1,12 +1,13 @@
-"use client";
-import { useState } from "react";
-import { Slide, ToastContainer, toast } from "react-toastify";
+'use client';
+import { toastSuccess } from '@/utils/toastUtils';
+import { useState } from 'react';
+import { Slide, ToastContainer, toast } from 'react-toastify';
 // import tostify css
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 const CreateWebSearch = () => {
   //handle the form submission
-  const [siteName, setSiteName] = useState("");
+  const [siteName, setSiteName] = useState('');
   const [isSiteAvailable, setIsSiteAvailable] = useState(false);
 
   //handle input siteNameChange
@@ -17,31 +18,22 @@ const CreateWebSearch = () => {
     event.preventDefault();
     // Do something with the form data, such as sending it to your server
     checkSite(siteName);
-    console.log("Form submitted with sitename:", siteName);
+    console.log('Form submitted with sitename:', siteName);
   };
 
   const checkSite = async (siteName: string) => {
-    const response = await fetch("/api/site-check", {
-      method: "POST",
+    const response = await fetch('/api/site-check', {
+      method: 'POST',
       body: JSON.stringify({ siteName }),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     });
     const data = await response.json();
-    console.log("Site check response:", data.status);
+    console.log('Site check response:', data.status);
     setIsSiteAvailable(data.status);
     if (data.status) {
-      toast.success(data.message, {
-        theme: "dark",
-        position: toast.POSITION.TOP_RIGHT,
-        autoClose: 3000, //3 seconds
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        transition: Slide
-      });
+      toast.success(data.message, toastSuccess);
     }
     // Do something with the response data, such as updating the UI
   };
@@ -49,16 +41,16 @@ const CreateWebSearch = () => {
   return (
     <>
       {isSiteAvailable ? <ToastContainer /> : <p>Site is not available</p>}
-      <div className="form-control flex flex-row items-center justify-center">
+      <div className='form-control flex flex-row items-center justify-center'>
         <input
-          type="text"
-          placeholder="Create your own unique URL."
-          className="input input-bordered w-24 md:w-auto"
+          type='text'
+          placeholder='Create your own unique URL.'
+          className='input input-bordered w-24 md:w-auto'
           value={siteName}
           onChange={handleSiteChange}
         />
-        <button className="btn btn-primary ml-2" onClick={handleClick}>
-          Let's Encrypt!{" "}
+        <button className='btn btn-primary ml-2' onClick={handleClick}>
+          Let's Encrypt!{' '}
         </button>
       </div>
     </>
